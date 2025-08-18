c2fe60ce927f875098e5a79ab73b913bf2d46b4c
Jhordan <jcaballerooros@gmail.com>
2025-08-17 21:32:00 -0500

    se creo el repositorio para el curso

---
+++ src/main/java/com/mycompany/programacion_orientada_a_objetos/Programacion_orientada_a_objetos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacion_orientada_a_objetos;
+
+/**
+ *
+ * @author jcaba
+ */
+public class Programacion_orientada_a_objetos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
