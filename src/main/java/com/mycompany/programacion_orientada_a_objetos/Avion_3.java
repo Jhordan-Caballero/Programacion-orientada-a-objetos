38f39ca11aece38e8c0d4e5725ecfc529543802b
Jhordan <jcaballerooros@gmail.com>
2025-10-12 21:38:16 -0500

    semana 9

--- src/main/java/com/mycompany/programacion_orientada_a_objetos/Avion_3.java
+++ src/main/java/com/mycompany/programacion_orientada_a_objetos/Avion_3.java
@@ -8,6 +8,31 @@ package com.mycompany.programacion_orientada_a_objetos;
  *
  * @author jcaba
  */
-public class Avion_3 {
-    
+interface Avion_3 {
+    void despegar(); 
+    void aterrizar();
+    default void informacion (){
+        System.out.println("Este avion es generico");
+    }      
+}
+
+class Avionturistico implements Avion_3 {
+    private String modelo;
+    private int capacidad;
+    public Avion_turistico (String modelo, int capacidad){
+        this.modelo = modelo;
+        this.capacidad = capacidad;         
+    }
+}
+@Override 
+public void despegar (){
+ System.out.println("Avion turistico" + modelo "despegando con" + capacacidad + "de capacidad de paasajeros")
+}
+@Override 
+public void aterrizar (){
+System.out.println("Avion turistico" + modelo "aterrizando suavemente")
+}
+@Override 
+public void informacion (){
+System.out.println("Avion turistico modelo = " + modelo "con capacidad de " + capacacidad )
 }
